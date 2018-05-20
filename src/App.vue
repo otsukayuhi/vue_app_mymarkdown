4f507296165e23c9e62403021f77a7d3e54c38cc
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2018-05-20 18:49:29 +0900

    add new component and set auth google

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="add">
+  <!-- 条件付きレンダリング -->
+  <Home v-if="!isLogin"></Home>
+  <Editor v-if="isLogin"></Editor>
+</div>
 </template>
 
 <script>
+// .vueファイル読み込み
+import Home from './components/Home';
+import Editor from './components/Editor';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isLogin: false // Home or Editor 画面切り替え
     }
+  },
+  // createdは、Vue.jsがそのコンポーネントを作ったタイミングで、実行される。
+  created: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.isLogin = true;
+      } else {
+        this.isLogin = false;
+      }
+      console.log(this.isLogin);
+
+    });
+  },
+  components: {
+    // コンポーネントの定義 → <template>内でHTMLタグの用に使える
+    'Home': Home,
+    'Editor': Editor
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="home">
+  <h1>{{ msg }}</h1>
+  <!-- @clickで、googleLogin関数を実行 -->
+  <button @click="googleLogin">Google アカウントでログイン</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to MyMarkdown'
+    }
+  },
+  methods: {
+    googleLogin: function() {
+      // Googleにログイン
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
+  }
+}
+</script>
