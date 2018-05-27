7a4411c9dfd60146bf9a8839cba7b711ce1c818c
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2018-05-27 19:29:22 +0900

    update add memo setting

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,19 @@
 <template>
-<div class="editor">
+<div id="editor">
   <h1>エディター画面</h1>
+  <span class="editor_img"><img v-bind:src="user.photoURL"></span>
   <span>{{ user.displayName }}</span>
   <span>{{ user.email }}</span>
-  <span class="editor_img"><img v-bind:src="user.photoURL"></span>
   <button @click="logout">ログアウト</button>
-  <div class="editorWrap">
-    <textarea class="markdown" v-model="markdown"></textarea>
+  <div class="editorWrapper">
+    <div class="memoListWrapper">
+      <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" :data-selected="index === selectedIndex">
+        <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+      </div>
+      <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+      <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+    </div>
+    <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
     <div class="preview" v-html="preview()"></div>
   </div>
 </div>
@@ -14,42 +21,92 @@
 
 <script>
 import marked from 'marked';
+
 export default {
   name: 'editor',
-  props: ['user'], // 親コンポーネントから、userを受け継ぐ。
-  data () {
+  props: ['user'],
+  data() {
     return {
-      markdown: ''
+      memos: [{
+        markdown: ''
+      }],
+      selectedIndex: 0
     }
   },
   methods: {
     logout: function() {
       firebase.auth().signOut();
     },
+    addMemo: function() {
+      this.memos.push({
+        markdown: '無題のメモ',
+      });
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
     preview: function() {
-      return marked(this.markdown);
-    }
+      return marked(this.memos[this.selectedIndex].markdown);
+    },
+    displayTitle: function(text) {
+      return text.split(/\n/)[0].replace(/#\s/, '');
+    },
+    deleteMemo: function() {
+      this.memos.splice(this.selectedIndex, 1);
+      if (this.selectedIndex > 0) {
+        this.selectedIndex--;
+      }
+    },
   }
 }
+
 </script>
 
 <style lang="scss" scoped>
+.editorWrapper{
+  display: flex;
+}
 .editor_img {
-  display: block;
+  display: inline-block;
   width: 50px;
-  height: 50px;
   img {
     width: 100%;
   }
 }
-.editorWrap {
-  display: flex;
+.memoListWrapper {
+  width: 20%;
+  border-top: 1px solid #000;
+}
+.memoList {
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: left;
+  border-bottom: 1px solid #000;
+  &:nth-child(even) {
+      background-color: #ccc;
+  }
+  &[data-selected="true"] {
+      background-color: #ccf;
+  }
+}
+.memoTitle {
+  height: 1.5em;
+  margin: 0;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.addMemoBtn {
+  margin-top: 20px;
+}
+.deleteMemoBtn {
+  margin: 10px;
 }
 .markdown {
-  width: 50%;
+  width: 40%;
   height: 500px;
 }
 .preview {
-  width: 50%;
+  width: 40%;
+  text-align: left;
 }
 </style>
